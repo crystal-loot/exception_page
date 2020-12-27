cb6e91141bf4d87e85a001ccd94621ef3d17f077
Sijawusz Pur Rahnama <sija@sija.pl>
2020-12-27 21:09:50 +0100

    Use backtracer gem instead of manual parsing

--- spec/frame_spec.cr
+++ spec/frame_spec.cr
@@ -1,27 +1,29 @@
 require "./spec_helper"
 
+include ExceptionPage::Helpers
+
 describe "Frame parsing" do
   it "returns the correct label" do
-    frame = frame_for("from usr/crystal-lang/frame_spec.cr:6:7 in '->'")
-    frame.label.should eq("crystal")
+    frame = frame_for("usr/crystal-lang/frame_spec.cr:6:7 in '->'")
+    label_for_frame(frame).should eq("crystal")
 
-    frame = frame_for("from usr/crystal/frame_spec.cr:6:7 in '->'")
-    frame.label.should eq("crystal")
+    frame = frame_for("usr/crystal/frame_spec.cr:6:7 in '->'")
+    label_for_frame(frame).should eq("crystal")
 
-    frame = frame_for("from lib/exception_page/spec/frame_spec.cr:6:7 in '->'")
-    frame.label.should eq("exception_page")
+    frame = frame_for("lib/exception_page/spec/frame_spec.cr:6:7 in '->'")
+    label_for_frame(frame).should eq("exception_page")
 
-    frame = frame_for("from lib/exception_page/frame_spec.cr:6:7 in '->'")
-    frame.label.should eq("exception_page")
+    frame = frame_for("lib/exception_page/frame_spec.cr:6:7 in '->'")
+    label_for_frame(frame).should eq("exception_page")
 
-    frame = frame_for("from lib/frame_spec.cr:6:7 in '->'")
-    frame.label.should eq("app")
+    frame = frame_for("lib/frame_spec.cr:6:7 in '->'")
+    label_for_frame(frame).should eq("app")
 
-    frame = frame_for("from src/frame_spec.cr:6:7 in '->'")
-    frame.label.should eq("app")
+    frame = frame_for("src/frame_spec.cr:6:7 in '->'")
+    label_for_frame(frame).should eq("app")
   end
 end
 
 private def frame_for(backtrace_line)
-  ExceptionPage::FrameGenerator.generate_frames(backtrace_line).first
+  Backtracer.parse(backtrace_line).frames.first
 end
