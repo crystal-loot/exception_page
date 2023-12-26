e09f8d8ad5620ccf39adc57ce341b1ef1f0e2390
Sijawusz Pur Rahnama <sija@sija.pl>
2023-12-26 17:03:42 +0100

    Show error message details only when needed (#46)
    * Show error message details only when needed
    
    * Update specs

--- spec/support/test_handler.cr
+++ spec/support/test_handler.cr
@@ -2,14 +2,22 @@ class TestHandler
   include HTTP::Handler
 
   def call(context)
-    if context.request.resource == "/favicon.ico"
+    case context.request.resource
+    when "/favicon.ico"
       context.response.print ""
+    when "/multiline-exception"
+      begin
+        raise CustomException.new("Something went very wrong\nBut wait, there's more!")
+      rescue e : CustomException
+        context.response.content_type = "text/html"
+        context.response.print MyApp::ExceptionPage.for_runtime_exception(context, e)
+      end
     else
       begin
         raise CustomException.new("Something went very wrong")
       rescue e : CustomException
         context.response.content_type = "text/html"
-        context.response.print MyApp::ExceptionPage.for_runtime_exception(context, e).to_s
+        context.response.print MyApp::ExceptionPage.for_runtime_exception(context, e)
       end
     end
   end
