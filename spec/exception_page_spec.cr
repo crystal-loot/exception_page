ae2ba333abb650c8ac711845fecb4af0e474c350
Sijawusz Pur Rahnama <sija@sija.pl>
2023-12-26 03:55:29 +0100

    Update specs

--- spec/exception_page_spec.cr
+++ spec/exception_page_spec.cr
@@ -9,6 +9,13 @@ describe ExceptionPage do
     flow.show_all_frames
     flow.should_be_able_to_view_other_frames
   end
+
+  it "allows debugging the multiline exception page" do
+    flow = MultilineErrorDebuggingFlow.new
+
+    flow.view_error_page
+    flow.should_have_additional_message_lines
+  end
 end
 
 class ErrorDebuggingFlow < LuckyFlow
@@ -19,8 +26,6 @@ class ErrorDebuggingFlow < LuckyFlow
   def should_have_information_for_debugging
     current_page.should have_element("@exception-title", text: "Something went very wrong")
     current_page.should have_element("@code-frames", text: "test_server.cr")
-    click("@see-raw-error-message")
-    current_page.should have_element("@raw-error-message")
   end
 
   def show_all_frames
@@ -30,6 +35,22 @@ class ErrorDebuggingFlow < LuckyFlow
   def should_be_able_to_view_other_frames
     el("@code-frame-file", "server.cr").click
     current_page.should have_element("@code-frame-summary", text: "->")
+  end
+
+  # A shim used for readibility in tests
+  private def current_page
+    self
+  end
+end
+
+class MultilineErrorDebuggingFlow < LuckyFlow
+  def view_error_page
+    visit "/multiline-exception"
+  end
+
+  def should_have_additional_message_lines
+    click("@see-raw-error-message")
+    current_page.should have_element("@raw-error-message")
   end
 
   # A shim used for readibility in tests
